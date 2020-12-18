bcc53b58007fa47a9a89d9394063fc7bb3bde54f
IhsenBen <72970267+IhsenBen@users.noreply.github.com>
2020-12-18 11:21:32 +0100

    Update devinette.js

--- devinette.js
+++ devinette.js
@@ -1,15 +1,19 @@
 function deviner(){
     let lesserlimit=10;
     let biggerlimit=20;
+    let compteur=1;
     let usertry=document.getElementById("devinette").value;
     if (biggerlimit<usertry){
+        compteur++;
         document.getElementById("message_devi").innerHTML="plus Petit!";
+        
     }
     else if (usertry < lesserlimit){
+        compteur++;
         document.getElementById("message_devi").innerHTML="plus Grand!";
     }
      else{
-        document.getElementById("message_devi").innerHTML="Bravo! WOOOOw! T'es Genial Alexis!";
+        document.getElementById("message_devi").innerHTML="Bravo! WOOOOw! Vous Avez trouve en" + compteur +"essais! T'es Genial Alexis! :)";
         
     }
 };
